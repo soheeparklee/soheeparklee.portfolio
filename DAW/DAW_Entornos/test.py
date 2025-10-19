6fc9d24bbbfc5935cc027c3896d8de18a4a81113
soheeparklee <soheeparklee@gmail.com>
2025-10-20 00:21:07 +0200

    Clean git repo

--- DAW/DAW_Entornos/test.py
+++
@@ -1,9 +0,0 @@
-print("Introduce el primer número: ")
-numero1 = int(input())
-print("Introduce el segundo número: ")
-numero2 = int(input())
-
-total = numero1 + numero2
-print("El resultado es: " , total)
-
-
